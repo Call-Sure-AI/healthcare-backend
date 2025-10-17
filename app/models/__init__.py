09b7453122b9cd63e49bcfc0e774731f5b52c6ea
Souvik-prog <gsouvik2807@gmail.com>
2025-10-17 22:47:40 +0530

    Revert "All commits for Healthsure Backend"
    This reverts commit c6e2d84a48037183c38370a5877ebbab84de771f.

--- app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.doctor import Doctor
-from app.models.appointment import Appointment
-from app.models.leave import DoctorLeave
-from app.models.call_session import CallSession  # NEW
-
-__all__ = ["Doctor", "Appointment", "DoctorLeave", "CallSession"]
